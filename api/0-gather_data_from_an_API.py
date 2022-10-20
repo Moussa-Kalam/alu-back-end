173750683574b519e9e165c65a7c751a9b5171a2
Moussa-Kalam <m.amzat@alustudent.com>
2022-10-20 23:58:44 +0200

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,34 +1,38 @@
 #!/usr/bin/python3
 
-"""Python script that returns information about an employee's TODO list progress"""
+"""Import libraries"""
 
 import requests
 import sys
 
-user_id = int(sys.argv[1])
-todos_url = "https://jsonplaceholder.typicode.com/todos"
-users_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
 
-todo_data = requests.get(todos_url).json()
+"""Import libraries"""
 
-employee_name = requests.get(users_url).json()["name"]
+if __name__ == "__main__":
+    user_id = int(sys.argv[1])
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    users_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
 
-total_user_todos = 0
-completed_todos = 0
-titles = []
-for todo in todo_data:
-    # Check if user_id is the same input as parameter
-    if user_id == todo["userId"]:
-        # Get the total number of todos
-        total_user_todos += 1
-        # Get the total number of completed tasks
-        if todo["completed"]:
-            completed_todos += 1
-            # Get the titles of the completed tasks
-            titles.append(todo["title"])
+    todo_data = requests.get(todos_url).json()
 
-print("Employee {} is done with tasks({}/{})"
-      .format(employee_name, completed_todos, total_user_todos, titles))
+    employee_name = requests.get(users_url).json()["name"]
 
-for title in titles:
-    print("\t {}".format(title))
+    total_user_todos = 0
+    completed_todos = 0
+    titles = []
+    for todo in todo_data:
+        # Check if user_id is the same input as parameter
+        if user_id == todo["userId"]:
+            # Get the total number of todos
+            total_user_todos += 1
+            # Get the total number of completed tasks
+            if todo["completed"]:
+                completed_todos += 1
+                # Get the titles of the completed tasks
+                titles.append(todo["title"])
+
+    print("Employee {} is done with tasks({}/{})"
+        .format(employee_name, completed_todos, total_user_todos, titles))
+
+    for title in titles:
+        print("\t {}".format(title))
